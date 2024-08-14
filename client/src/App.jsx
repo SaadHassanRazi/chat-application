8c18a26885819849e9b264d16f590245849ac9bc
SaadHassanRazi <saadhassanrazi@gmail.com>
2024-08-14 15:31:03 +0500

    initial structure

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Routes from "./frameworkUI/routes/Routes";
+const App = () => {
+  return (
+    <>
+    <Routes/> 
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/frameworkUI/exports/Exports.js
@@ -0,0 +1,3 @@
+export { default as ChannelContainer } from "../../interface/components/channelContainer/ChannelContainer";
+export { default as ChannelListContainer } from "../../interface/components/channelListContainer/ChannelListContainer";
+    
