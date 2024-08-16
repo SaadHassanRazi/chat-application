167726c982ae31790b64beb4e4714f60af1fdfd3
SaadHassanRazi <saadhassanrazi@gmail.com>
2024-08-16 14:49:49 +0500

    basic setup

--- client/src/frameworkUI/exports/Exports.js
+++
@@ -1,3 +0,0 @@
-export { default as ChannelContainer } from "../../interface/components/channelContainer/ChannelContainer";
-export { default as ChannelListContainer } from "../../interface/components/channelListContainer/ChannelListContainer";
-    
