1bfe52569ac3e214959e78eb8ff13aa97a467b3c
SaadHassanRazi <saadhassanrazi@gmail.com>
2024-08-23 14:33:58 +0500

    client login page

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { useMutation, UseMutationResult } from "@tanstack/react-query";
 import axios, { AxiosResponse } from "axios";
 import { createContext, ReactNode, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 type AuthContext = {
   signup: UseMutationResult<AxiosResponse, unknown, User>;
 };
@@ -18,9 +19,22 @@ type AuthProviderProps = {
   children: ReactNode;
 };
 const AuthProvider = ({ children }: AuthProviderProps) => {
+  const navigate = useNavigate();
   const signup = useMutation({
     mutationFn: (user: User) => {
       return axios.post(`${import.meta.env.VITE_SERVER_URL}/signup`, user);
+    },
+    onSuccess() {
+      navigate("/login");
+    },
+  });
+  const login = useMutation({
+    mutationFn: (id: string) => {
+      return axios
+        .post(`${import.meta.env.VITE_SERVER_URL}/login`, { id })
+        .then((res) => {
+          res.data as { token: string; user: User };
+        });
     },
   });
   return <Context.Provider value={{ signup }}>{children}</Context.Provider>;
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,42 @@
+import { FormEvent, useRef } from "react";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import { useAuth } from "../context/AuthContext";
 
 const Login = () => {
+  const { login } = useAuth();
+  
+  const usernameRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (e: FormEvent) => {
+    e.preventDefault();
+    if (login.isPending) return;
+    const username = usernameRef.current?.value;
+
+    if (username == null || username === "" ) {
+      return;
+    }
+    login.mutate({ id: username});
+  };
   return (
     <div>
-      Login
+      <h1 className="text-3xl text-bold mb-8 text-center">Signup</h1>
+      <form
+        onSubmit={submitHandler}
+        className="grid grid-cols-[auto,1fr] gap-x-3 gap-y-5 items-center justify-items-end"
+      >
+        <label htmlFor="userName">Username</label>
+        <Input id="userName" required ref={usernameRef} />
+        <Button
+          disabled={login.isPending}
+          className="col-span-full"
+          type="submit"
+        >
+          {login.isPending ? "Loading" : "Login"}
+        </Button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
